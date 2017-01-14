a8ddb9a217aeff00cef43c327f5f61a45377bb0c
Marco <marcomartinezavila@gmail.com>
2017-01-14 16:43:48 +0100

    The automatic pairing proccess has been completed.

--- src/main/java/zowicommunication/zowi/marco/zowicommunication/MainActivity.java
+++ src/main/java/zowicommunication/zowi/marco/zowicommunication/MainActivity.java
@@ -1,18 +1,38 @@
 package zowicommunication.zowi.marco.zowicommunication;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
     private Set<BluetoothDevice> pairedDevices;
+    private BluetoothSocket bluetoothSocket;
+    private BluetoothReceiver bluetoothReceiver;
+
+    public static int REQUEST_BLUETOOTH = 1;
+    private static final int REQUEST_COARSE_LOCATION = 2;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +40,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBluetoothConnectivity(bluetoothAdapter);
 
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth Device Not Available", Toast.LENGTH_SHORT).show();
-        }
-        else if (!bluetoothAdapter.isEnabled()) {
-            Toast.makeText(this, "Habilita el bluetooth, capullo", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(this, "Todo correcto", Toast.LENGTH_SHORT).show();
-        }
+        checkLocationPermission();
 
-        getPairedDevices();
+        //getPairedDevices();
     }
 
-    private void getPairedDevices() {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bluetoothReceiver);
+    }
+
+    private void checkLocationPermission() {
+        int accessCoarseLocation = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (accessCoarseLocation != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION);
+        }
+        else {
+            startDiscovery();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startDiscovery();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void startDiscovery() {
+        bluetoothReceiver = new BluetoothReceiver();
+
+        IntentFilter bluetoothFilter = new IntentFilter();
+        bluetoothFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        bluetoothFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+
+        registerReceiver(bluetoothReceiver, bluetoothFilter);
+
+        bluetoothAdapter.startDiscovery();
+    }
+
+    private void checkBluetoothConnectivity(BluetoothAdapter bluetoothAdapter) {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth no disponible", Toast.LENGTH_SHORT).show();
+        }
+        else if (!bluetoothAdapter.isEnabled()) {
+            //Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+            bluetoothAdapter.enable();
+        }
+    }
+
+    public void getPairedDevices(View v) {
         pairedDevices = bluetoothAdapter.getBondedDevices();
 
         if (pairedDevices.size() > 0) {
@@ -43,7 +111,40 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         else {
-            Toast.makeText(this, "No paired devices", Toast.LENGTH_SHORT).show();
+            Log.i("Devices", "No paired devices");
+        }
+    }
+
+    private class BluetoothReceiver extends BroadcastReceiver {
+        private static final String ZOWI_NAME = "Zowi";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_STARTED)) {
+                Log.i("BluetoothReceiver", "Discovery started");
+            }
+            else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+                Log.i("BluetoothReceiver", "Discovery finished");
+            }
+            else if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if (device.getName().equals(ZOWI_NAME)) {
+                    device.createBond();
+                }
+                Log.i("BluetoothReceiver", "Device discovered " + device.getName() + ": " + device.getAddress());
+            }
+            else if (action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String name = device.getName();
+                int bondState = device.getBondState();
+
+                if (name.equals(ZOWI_NAME) && (bondState == BluetoothDevice.BOND_BONDED)) {
+
+                }
+            }
         }
     }
 }
